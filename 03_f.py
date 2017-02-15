449663b03c555e1e620fcf118e509a4a38253224
Pepe Valdivia <jvaldivia@softweb.pe>
2017-02-14 21:12:10 -0500

    Pregunta 3

---
+++ 03_f.py
@@ -0,0 +1,22 @@
+#Determinar que valor se imprimiría en la consola
+
+numerador = 26
+denominador = 5
+
+x = 26//5
+y = 26%5
+
+lenguajes = ['PHP', 'Perl', 'Python']
+
+if x < y:
+	lenguajes.push('Lua')
+else:
+	lenguajes.pop(5)
+
+print(lenguajes)
+
+
+'''
+RPTA:
+['PHP', 'Perl']
+'''
